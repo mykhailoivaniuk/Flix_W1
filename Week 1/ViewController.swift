6abb121cf03607bb59151546f031a5e475950b0f
Misha Ivaniuk <mykhailo.ivaniuk@gmail.com>
2020-09-21 20:28:14 -0500

    Initial Commit

---
+++ Week 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Week 1
+//
+//  Created by Mykhailo Ivaniuk on 9/21/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
