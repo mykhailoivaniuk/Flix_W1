a0a1afe0e599e78d634e90b7937b13e0d1cf03a8
Misha Ivaniuk <mykhailo.ivaniuk@gmail.com>
2020-09-24 11:08:15 -0500

    Updated: Auto-layout added, styled launch screen, movie poster for each movie

---
+++ Week 1/MovieCellTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieCellTableViewCell.swift
+//  Week 1
+//
+//  Created by Mykhailo Ivaniuk on 9/21/20.
+//
+
+import UIKit
+
+class MovieCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
