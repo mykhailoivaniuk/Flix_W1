333781313d895bcc803b0b070837bd773e6cfc51
Misha Ivaniuk <mykhailo.ivaniuk@gmail.com>
2020-10-02 03:06:18 -0500

    Tab_Bar Navigation added; collection view added

---
+++ Week 1/MovieDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MovieDetailsViewController.swift
+//  Week 1
+//
+//  Created by Mykhailo Ivaniuk on 10/2/20.
+//
+
+import UIKit
+
+class MovieDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Week 1/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Week 1
+//
+//  Created by Mykhailo Ivaniuk on 10/2/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
